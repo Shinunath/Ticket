e6553123fc9118c06eede124e053c527a3c5dfa2
Shinunath <nathshinu71@gmail.com>
2026-01-29 20:16:33 +0530

    Improve logging for MongoDB connection by adding a label to the URL output

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 let ConnectDB = async () => {
     try {
-        console.log(process.env.MONGOURL)
+        console.log("MONGODB_URL", process.env.MONGOURL)
         await mongoose.connect(process.env.MONGOURL);
         console.log("Database connected successfully")
     } catch (error) {
